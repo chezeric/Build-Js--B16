d4dfd0bb5eb6c04b5f1ec641d79be70bec3e59b7
Eric Livingston <chezeric@comcast.net>
2014-06-05 11:53:29 -0700

    Day 2 added mp3 file and added number limitations and bleep

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 // js
-var deleteBoxes = function(){
-  var clear = document.getElementById("box");
-  document.body.removeChild(clear);
+var boxes = [];
+ var bleep = new Audio('bleep.mp3');
+var createBoxes = function() {
+  var boxCount = boxes.length;
+  var remaining = (10 - boxes.length);
+
+  if (remaining === 0) {
+    alert("No more space remains.");
+    return false;
+  }
+
+  var number = prompt("How many do you want? " + remaining + " slots left.");
+
+  if(number <= remaining) {
+    var color = prompt("Input a color choice").toLowerCase();
+
+    for(var i=0; i < number; i++) {
+      var div = document.createElement('div');
+      var box = document.querySelector('#boxes').appendChild(div);
+      box.className = "box";
+      box.style.backgroundColor = color;
+      box.id = "box" + boxes.length;
+      var boxID = box.id;
+      boxes.push(boxID);
+    }
+  } else {
+      alert("Must be " + remaining + " or less");
+  }
 };
 
-var createBoxes = function() {
-  var number = prompt("How many do you want? (1-10)");
+var deleteBoxes = function() {
 
-  if(number <= 10){
-    var color = prompt("Input a color choice").toLowerCase();
-   for(var i=0; i < number; i++){
-    var create = document.createElement('div');
-    var box = document.body.appendChild(create);
-    box.className = "box";
-     box.id = "box";
-     box.style.backgroundColor = color;
-   }
+  if (document.querySelector('#boxes').hasChildNodes() === false) {
+    alert ("Dale, there aren't any more boxes to delete!!");
   } else {
-    alert("Enter a number ten or less");
+    bleep.play(bleep.mp3);
+    var clear = document.querySelector('#boxes');
+  clear.removeChild(clear.lastChild);
+  boxes.pop();
   }
 };
